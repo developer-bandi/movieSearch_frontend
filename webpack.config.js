5fc950e6169af5074beb003c31bea05b60c05a17
김상두 <puki4416@gmail.com>
2023-01-09 21:37:42 +0900

    chore: webpack이 png 파일도 읽을수 있도록 수정

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
       },
       { test: /\.(js|jsx)$/, loader: "babel-loader", exclude: /node_modules/ },
       {
-        test: /\.jpg$/,
+        test: /\.(jpg|png)$/,
         use: [
           {
             loader: "file-loader",
