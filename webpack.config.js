a4af81213915ee169278ea8e4b472bc214e29d89
김상두 <puki4416@gmail.com>
2023-01-09 15:39:54 +0900

    chore: css module 설정추가

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,15 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
       },
       { test: /\.(js|jsx)$/, loader: "babel-loader", exclude: /node_modules/ },
       {
