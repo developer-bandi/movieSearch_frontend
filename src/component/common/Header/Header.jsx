5fff2d3fd3a147cbe5e6412d70373695148b9db9
김상두 <puki4416@gmail.com>
2023-01-10 13:22:10 +0900

    feat: common/Header 컴포넌트 구현

---
+++ src/component/common/Header/Header.jsx
@@ -0,0 +1,19 @@
+/** @jsx h */
+import h from "../../../../lib/react";
+import styles from "./Header.module.css";
+import "../../../img/icon.png";
+
+const Header = () => {
+  return (
+    <header class={styles.mainBlock}>
+      <div class={styles.subBlock}>
+        <div class={styles.logoBlock}>
+          <img class={styles.logoIcon} src="./icon.png"></img>
+          <div class={styles.logoWord}>MovieSearch</div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
