86b0cb54215744de1927c95b56fddbc3ff2bd9eb
김상두 <puki4416@gmail.com>
2023-01-11 00:21:03 +0900

    feat: Result/PosterList/Poster 컴포넌트 구현

---
+++ src/component/Result/PosterList/Poster/Poster.jsx
@@ -0,0 +1,35 @@
+/** @jsx h */
+import h from "../../../../../lib/react";
+import { Link } from "../../../../../lib/react-router-dom";
+import styles from "./Poster.module.css";
+import "../../../../img/nullmovie.webp";
+
+const Poster = ({ title, id, posterPath, rate, release }) => {
+  return (
+    <div class={styles.mainBlock}>
+      <Link class={styles.subBlock} href={`/detail/${id}`}>
+        <img
+          class={styles.posterBlock}
+          src={
+            posterPath
+              ? `https://image.tmdb.org/t/p/w500${posterPath}`
+              : "./nullmovie.webp"
+          }
+        ></img>
+        <div class={styles.infoBlock}>
+          <div class={styles.title}>{title}</div>
+          <div class={styles.infoRowBlock}>
+            <i class={styles.subTitle}>개봉</i>
+            {release}
+          </div>
+          <div class={styles.infoRowBlock}>
+            <i class={styles.subTitle}>평점</i>
+            {rate}
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Poster;
