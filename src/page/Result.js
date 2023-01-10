003b682697fbce3a047b1e8e0bf46e0a724ae1a5
김상두 <puki4416@gmail.com>
2023-01-11 00:22:53 +0900

    feat: Result 페이지 구현

--- src/page/Result.js
+++ src/page/Result.js
@@ -1,8 +1,21 @@
 /** @jsx h */
 import h from "../../lib/react";
+import Footer from "../component/common/Footer/Footer";
+import Header from "../component/common/Header/Header";
+import Keyword from "../component/Result/Keyword";
+import PosterListContainer from "../component/Result/PosterList/PosterListContainer";
 
 const Result = () => {
-  return <div>result페이지</div>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Keyword />
+        <PosterListContainer />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Result;
