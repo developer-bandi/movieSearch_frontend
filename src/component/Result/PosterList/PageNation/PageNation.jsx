3e856af0d1742886fcc1daa75a7b386146d08d0a
김상두 <puki4416@gmail.com>
2023-01-11 00:21:59 +0900

    feat: Result/PosterList/PageNation 컴포넌트 구현

---
+++ src/component/Result/PosterList/PageNation/PageNation.jsx
@@ -0,0 +1,65 @@
+/** @jsx h */
+import h from "../../../../../lib/react";
+import styles from "./PageNation.module.css";
+
+const PageNation = ({
+  page,
+  changePage,
+  totalAmount,
+  addStyle,
+  pageUnit,
+  contentUnit,
+  allowPrevPageSet,
+  makePrevSetPage,
+  makePages,
+  allowNextPageSet,
+  makeNextSetPage,
+}) => {
+  return (
+    <ul class={styles.pageButtonList}>
+      <li key={"prev"}>
+        <div
+          class={`${styles.pageIcon} ${page > 5 ? "" : styles.ban}`}
+          onClick={() => {
+            if (allowPrevPageSet({ page, pageUnit }))
+              changePage(makePrevSetPage({ page, pageUnit }));
+          }}
+        >
+          &lt;
+        </div>
+      </li>
+      {makePages({ page, totalAmount, contentUnit, pageUnit }).map((cur) => {
+        return (
+          <li
+            class={`${styles.pageButton} ${
+              cur === page ? styles.selected : ""
+            }`}
+            onClick={() => {
+              changePage(cur);
+            }}
+            key={cur}
+          >
+            {cur}
+          </li>
+        );
+      })}
+      <li key={"next"}>
+        <div
+          class={`${styles.pageIcon} ${
+            allowNextPageSet({ page, totalAmount, contentUnit, pageUnit })
+              ? ""
+              : styles.ban
+          }`}
+          onClick={() => {
+            if (allowNextPageSet({ page, totalAmount, contentUnit, pageUnit }))
+              changePage(makeNextSetPage({ page, pageUnit }));
+          }}
+        >
+          &gt;
+        </div>
+      </li>
+    </ul>
+  );
+};
+
+export default PageNation;
---
+++ src/component/Result/PosterList/PageNation/PageNationContainer.jsx
@@ -0,0 +1,62 @@
+/** @jsx h */
+import h from "../../../../../lib/react";
+import PageNation from "./PageNation";
+
+const PageNationContainer = ({
+  page,
+  changePage,
+  totalAmount,
+  addStyle,
+  pageUnit,
+  contentUnit,
+}) => {
+  const allowPrevPageSet = ({ page, pageUnit }) => {
+    if (page < pageUnit) return false;
+    return true;
+  };
+
+  const makePrevSetPage = ({ page, pageUnit }) =>
+    (Math.floor((page - 1) / pageUnit) - 1) * pageUnit + 1;
+
+  const makePages = ({ page, totalAmount, contentUnit, pageUnit }) => {
+    const pageSetOrder = Math.ceil(page / pageUnit) - 1;
+    const pageCount = Math.ceil(
+      Math.min(
+        (totalAmount - pageSetOrder * pageUnit * contentUnit) / contentUnit,
+        pageUnit
+      )
+    );
+    const pages = new Array(pageCount)
+      .fill(0)
+      .map((_, index) => index + 1 + pageSetOrder * 5);
+
+    return pages;
+  };
+
+  const allowNextPageSet = ({ page, contentUnit, totalAmount, pageUnit }) => {
+    if (Math.ceil(page / pageUnit) * contentUnit * pageUnit < totalAmount)
+      return true;
+    return false;
+  };
+
+  const makeNextSetPage = ({ page, pageUnit }) =>
+    Math.ceil(page / pageUnit) * pageUnit + 1;
+
+  return (
+    <PageNation
+      allowPrevPageSet={allowPrevPageSet}
+      makePrevSetPage={makePrevSetPage}
+      makePages={makePages}
+      allowNextPageSet={allowNextPageSet}
+      makeNextSetPage={makeNextSetPage}
+      page={page}
+      changePage={changePage}
+      totalAmount={totalAmount}
+      addStyle={addStyle}
+      pageUnit={pageUnit}
+      contentUnit={contentUnit}
+    />
+  );
+};
+
+export default PageNationContainer;
