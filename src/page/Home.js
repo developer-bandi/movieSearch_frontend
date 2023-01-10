8ab1ec5e6beeb421bc4676fad6e09d7826081955
김상두 <puki4416@gmail.com>
2023-01-10 16:51:21 +0900

    feat: Home 페이지 작성

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,8 +1,23 @@
 /** @jsx h */
 import h from "../../lib/react";
+import Footer from "../component/common/Footer/Footer";
+import Header from "../component/common/Header/Header";
+import MainImg from "../component/Home/MainImg/MainImg";
+import PopularCardListContainer from "../component/Home/PopularCardList/PopularCardListContainer";
+import SearchInput from "../component/Home/SearchInput/SearchInput";
 
 const Home = () => {
-  return <div>home페이지</div>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <MainImg />
+        <SearchInput />
+        <PopularCardListContainer />
+        <Footer />
+      </main>
+    </div>
+  );
 };
 
 export default Home;
