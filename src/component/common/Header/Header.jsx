1bc40ed041c228175d02c3823a5cddfdc89de19c
김상두 <puki4416@gmail.com>
2023-01-11 16:04:24 +0900

    fix: header icon 제거 및 link 추가

--- src/component/common/Header/Header.jsx
+++ src/component/common/Header/Header.jsx
@@ -1,16 +1,15 @@
 /** @jsx h */
 import h from "../../../../lib/react";
 import styles from "./Header.module.css";
-import "../../../img/icon.png";
+import { Link } from "../../../../lib/react-router-dom";
 
 const Header = () => {
   return (
     <header class={styles.mainBlock}>
       <div class={styles.subBlock}>
-        <div class={styles.logoBlock}>
-          <img class={styles.logoIcon} src="/icon.png"></img>
-          <div class={styles.logoWord}>MovieSearch</div>
-        </div>
+        <Link class={styles.logo} href="/">
+          MovieSearch
+        </Link>
       </div>
     </header>
   );
