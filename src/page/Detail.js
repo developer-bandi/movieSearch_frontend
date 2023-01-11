e76194579ee42cb325697cf39df3287aa95aeaae
김상두 <puki4416@gmail.com>
2023-01-11 15:36:32 +0900

    feat: Detail page 구현

--- src/page/Detail.js
+++ src/page/Detail.js
@@ -1,8 +1,17 @@
 /** @jsx h */
 import h from "../../lib/react";
+import Footer from "../component/common/Footer/Footer";
+import Header from "../component/common/Header/Header";
+import DetailInfoContainer from "../component/Detail/DetailInfo/DetailInfoContainer";
 
 const Detail = () => {
-  return <div>detail페이지</div>;
+  return (
+    <div>
+      <Header />
+      <DetailInfoContainer />
+      <Footer />
+    </div>
+  );
 };
 
 export default Detail;
