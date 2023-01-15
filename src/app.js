884787776d40432cd7b69c56eddfc9a6324d6908
김상두 <puki4416@gmail.com>
2023-01-15 14:57:43 +0900

    fix: import 경로 대소문자 수정

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 /** @jsx h */
 import h from "../lib/react";
 import { Route, Routes } from "../lib/react-router-dom";
-import Detail from "./page/detail";
-import Home from "./page/home";
-import Result from "./page/result";
+import Detail from "./page/Detail";
+import Home from "./page/Home";
+import Result from "./page/Result";
 
 const App = () => {
   return (
