c908acba4e2283622f69c5c6280f5970e5f37668
김상두 <puki4416@gmail.com>
2023-03-11 21:57:52 +0900

    feat : 이미지를 필요한 부분만 preload 하도록 변경

--- src/component/Home/MoviePosterCardList/MoviePosterCardListContainer.jsx
+++ src/component/Home/MoviePosterCardList/MoviePosterCardListContainer.jsx
@@ -6,6 +6,7 @@ import MoviePosterCardList from "./MoviePosterCardList";
 const MoviePosterCardListContainer = () => {
   const page = useSelector((store) => store.mainPosterPage.page);
   const dispatch = useDispatch();
+  const [loadPage, setLoadPage] = useState([]);
   const [cardList, setCardList] = useState({ loading: true, error: false });
   useEffect(() => {
     if (cardList.loading) {
@@ -13,7 +14,10 @@ const MoviePosterCardListContainer = () => {
         .then((response) => response.json())
         .then((data) => {
           setCardList({ loading: false, error: false, content: data });
-          data.forEach(({ posterPath }) => {
+          loadPage[1] = 1;
+          loadPage[2] = 1;
+          setLoadPage([...loadPage]);
+          data.slice(0, 10).forEach(({ posterPath }) => {
             const image = new Image();
             image.src = `https://image.tmdb.org/t/p/w500${posterPath}`;
           });
@@ -26,6 +30,17 @@ const MoviePosterCardListContainer = () => {
 
   const changePage = (direction) => {
     dispatch("mainPosterPage", { type: direction });
+    if (page + 2 !== 1 && page + 2 <= Math.ceil(cardList.content.length / 5)) {
+      loadPage[page + 2] = 1;
+      cardList.content
+        .slice((page + 1) * 5, (page + 2) * 5)
+        .forEach(({ posterPath }) => {
+          const image = new Image();
+          console.log(posterPath);
+          image.src = `https://image.tmdb.org/t/p/w500${posterPath}`;
+        });
+      setLoadPage([...loadPage]);
+    }
   };
   return (
     <MoviePosterCardList
